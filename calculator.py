66091b7f877222ae5679fc31acaf19ddd8efa830
Steve-MM007 <stevemuvui@gmail.com>
2023-02-15 11:36:31 +0300

    Calc_if

--- calculator.py
+++ calculator.py
@@ -1,24 +1,35 @@
 #calculator
-num1 = eval(input("Enter 1st number: "))
-num2  = eval(input("Enter 2nd number: "))
+number1 = eval(input("Enter 1st number: "))
+number2  = eval(input("Enter 2nd number: "))
 
 operator = input("Enter operator: ")
-
 def add(num1,num2):
     result = num1 + num2
-    return result
+    print(result)
 
 def subtract(num1,num2):
     result = num1 - num2 
-    return result
+    print(result)
 
 def divide(num1,num2):
     result = num1/num2
-    return result
+    print(result)
 
 def multiply(num1,num2):
     result = num1*num2
-    return result
+    print(result)
+
+if operator == "+":
+    add(number1,number2)
+elif operator == "-":
+    subtract(number1,number2)
+elif operator == "/":
+    divide(number1,number2)
+elif operator == "X" or operator == "x" or operator == "*":
+    multiply(number1,number2)
+else:
+    print("Invalid operator.")
+    
 
 
 
