c117e43b901fda9c5a334f6cf0be7b2d409b608b
Steve-MM007 <stevemuvui@gmail.com>
2023-02-15 10:45:31 +0300

    Calculator functions

---
+++ calculator.py
@@ -0,0 +1,24 @@
+#calculator
+num1 = eval(input("Enter 1st number: "))
+num2  = eval(input("Enter 2nd number: "))
+
+operator = input("Enter operator: ")
+
+def add(num1,num2):
+    result = num1 + num2
+    return result
+
+def subtract(num1,num2):
+    result = num1 - num2 
+    return result
+
+def divide(num1,num2):
+    result = num1/num2
+    return result
+
+def multiply(num1,num2):
+    result = num1*num2
+    return result
+
+
+
